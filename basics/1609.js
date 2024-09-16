1e3c959b3e629619a9b68b5baee70154d1b2536f
Francisc0Lopes <francisco.alves.lopes.03@gmail.com>
2024-09-16 10:51:36 +0100

    .

--- basics/1609.js
+++ basics/1609.js
@@ -16,4 +16,68 @@ console.log("31" + 2)
 console.log("31" - 2)
 console.log(typeof ("31" - 2))
 
+const k = "21"
+const z = 21
+console.log(k == z)
+console.log(k === z)
 
+
+let array = [123,,"STR"]
+console.log(array.length)
+console.log(array)
+console.log(array[1])
+console.log(array[2])
+
+
+array[100]=100
+console.log(array.length)
+
+array.push("last")
+console.log(array.length)
+console.log(array[101])
+
+array[102] = function () {return "func in 102"}
+
+console.log(array.length)
+console.log(array[102]())
+console.log(typeof(array[102]()))
+
+// PODE SE ACRESCENTAR PROPRIEDADES A QUAISQUER OBJETOS
+
+array.name="XPTO"
+console.log(array.name)
+
+
+const std1= {studentName: "Filipe", number:12}
+console.log(std1)
+
+std1.email = "fartodisto@gmail.com"
+console.log(std1)
+
+delete std1.studentName
+console.log(std1)
+
+function createStudent(name, number){
+    const std= {}
+    std.studentName = name
+    std.number= number
+
+    return std
+}
+
+const std2 = createStudent("std2", 2)
+const std3 = createStudent("std3", 3)
+std2.email = "fartodisto2@gmail.com"
+console.log(std2)
+console.log(std3)
+
+function showProps( obj){
+    for(let p in obj){
+        console.log(p + " : " + obj[p])
+    }
+}
+showProps(std3) 
+
+console.log(std2.studentName)       
+console.log(std2["studentName"])
+//linha 81 e 82 igual
