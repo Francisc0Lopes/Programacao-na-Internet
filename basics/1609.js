ec60ed8f58d6a440c16b3580a6f4d97b1d7ff765
Francisc0Lopes <francisco.alves.lopes.03@gmail.com>
2024-09-16 10:28:20 +0100

    1609.js

---
+++ basics/1609.js
@@ -0,0 +1,17 @@
+console.log('Hello World')
+
+let a = "STR" //não é para usar o var
+console.log(a)
+console.log(typeof (a))
+
+let b
+console.log(b)
+console.log(typeof (b))
+
+b = null
+console.log(b)
+console.log(typeof (b))
+
+console.log("31" + 2)
+console.log("31" - 2)
+console.log(typeof ("31" - 2))
