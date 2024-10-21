b77cf56ff83a5747bf416ef17c5e1acde3ba4e75
Francisc0Lopes <francisco.alves.lopes.03@gmail.com>
2024-10-21 22:06:01 +0100

    .

--- basics/27092024.js
+++ basics/27092024.js
@@ -51,3 +51,35 @@ console.log(`this =`, this)
 f2.call(std, 1, 2)
 
 f2.apply(std, [1,2])
+
+
+//quando numa thread coloco um loop infinito vai bloquear ->pergunta teste
+
+//Sempre que tiver a trabalhar num modelo assincrono, ter atençáo as chamadas de funções devido ao tempo que cada uma tem
+
+
+function add ( a,b){
+    return a+b;
+    
+}
+
+console.log("Before add")
+const result = console.log(add(2,3))
+console.log("After add")
+processResult(result)
+
+function addCB( a,  b, cb){
+    cb(a+b)
+}
+
+console.log("Before addCB")
+
+console.log("After addCB")
+
+
+
+//FAZENDO EXPORT CONSIGO FAZER IMPORT NOUTRO LADO
+
+
+
+
